0174315debaf757ce15ffc792d203ea6fb185e31
johnmi54 <50969212+johnmi54@users.noreply.github.com>
2019-05-24 09:25:35 +0800

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+clear
+pkg install update
+pkg install upgrade
+apt update
+apt upgrade -y
+pkg install php -y
+pkg install curl -y
+pkg install nano -y
+echo
+echo -e "\e[1m\e[32m Subscribe :\e[33m Termux Tricks & Tut"
+echo
